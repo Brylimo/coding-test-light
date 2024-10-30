5bff486fae52ad8c7751c62f5a541ed522420b08
brylimo <icj0103@gmail.com>
2024-10-30 22:06:09 +0900

    solved P2018_수들의_합5

---
+++ src/P2018_수들의_합5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class P2018_수들의_합5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int j = 0;
+        int sum = 0;
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            while (j + 1 <= n && sum + j + 1 <= n) {
+                sum += j + 1;
+                j++;
+            }
+
+            if (sum == n) {
+                cnt += 1;
+            }
+
+            sum -= i;
+        }
+
+        System.out.println(cnt);
+    }
+}
