929b088ae5cff69578490efb49ca6814a036e941
brylimo <icj0103@gmail.com>
2024-11-01 14:26:14 +0900

    solved P11003_최소값_찾기

---
+++ src/P11003_최소값_찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class P11003_최소값_찾기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<Node> mydeque = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            while (!mydeque.isEmpty() && mydeque.getLast().value > now) {
+                mydeque.removeLast();
+            }
+            mydeque.addLast(new Node(now, i));
+
+            if (mydeque.getFirst().index <= i - l) {
+                mydeque.removeFirst();
+            }
+            bw.write(mydeque.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+        public int value;
+        public int index;
+
+        Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+}
