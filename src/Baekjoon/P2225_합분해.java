7337c92a03b9cb2c5e6973d29e2254ee797856b8
brylimo <icj0103@gmail.com>
2024-12-22 17:19:56 +0900

    solved P2225_합분해

--- src/Baekjoon/P2225_합분해.java
+++ src/Baekjoon/P2225_합분해.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.io.*;
 
 public class P2225_합분해 {
+    static final int MOD = 1000000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,6 +11,23 @@ public class P2225_합분해 {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        int[][] dp = new int[k + 1][n + 1];
 
+        // 초기화
+        for (int i = 0; i <= n; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+            }
+        }
+
+        System.out.println(dp[k][n]);
     }
 }
