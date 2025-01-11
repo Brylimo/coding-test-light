9ad47be6bdcd896743e2561e5c5e20d8bb1871ee
brylimo <icj0103@gmail.com>
2025-01-11 12:12:21 +0900

    P8980_택배

---
+++ src/Baekjoon/sdaf.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+import java.util.*;
+import java.io.*;
+
+public class sdaf {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+    }
+}
