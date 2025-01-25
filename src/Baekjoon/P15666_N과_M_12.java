4dcaf61225d66932db036d638820ac4450435983
brylimo <icj0103@gmail.com>
2025-01-25 17:36:48 +0900

    P15666_N과_M_12

---
+++ src/Baekjoon/P15666_N과_M_12.java
@@ -0,0 +1,70 @@
+package Baekjoon;
+import java.util.*;
+import java.io.*;
+
+public class P15666_N과_M_12 {
+    static int n, m;
+    static int[] arr;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+    static ArrayList<Integer> candidates = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    static void calculate(int idx) {
+        if (candidates.size() == m) {
+            sb = new StringBuilder();
+            for (int i = 0; i < candidates.size(); i++) {
+                sb.append(candidates.get(i)).append(" ");
+            }
+
+            if (!set.contains(sb.toString())) {
+                set.add(sb.toString());
+            }
+
+            return;
+        }
+
+        if (idx == n) {
+            if (candidates.size() == m) {
+                sb = new StringBuilder();
+                for (int i = 0; i < candidates.size(); i++) {
+                    sb.append(candidates.get(i)).append(" ");
+                }
+
+                if (!set.contains(sb.toString())) {
+                    set.add(sb.toString());
+                }
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (candidates.size() == 0 || candidates.get(candidates.size() - 1) <= arr[i]) {
+                candidates.add(arr[i]);
+                calculate(idx + 1);
+                candidates.remove(candidates.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        calculate(0);
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
