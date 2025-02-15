87bae0e117278fbeda931120d29c3afd6c44653d
brylimo <icj0103@gmail.com>
2025-02-15 10:53:08 +0900

    Solved_2105_디저트_카페

--- src/SWEA/Solved_2105_디저트_카페.java
+++ src/SWEA/Solved_2105_디저트_카페.java
@@ -1,8 +1,17 @@
 package SWEA;
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
+/**
+ * Solved_2105_디저트_카페
+ * 난이도 3/10
+ * 구현
+ * 519ms
+ *
+ * n이 20까지로 충분히 작기 때문에 모든 좌표마다 모든 마름모를 그려 같은게 있으면 넣어주고 정답을 갱신하도록 했다.
+ * 시간을 줄이기 위해 디저트를 포함할 때 HashSet을 사용해 넣어주었다.
+ * 이미 HashSet에 포함된 디저트가 있거나 grid 범위를 벗어나면 카운트를 해주지 않고 바로 다음 경우의 수로 넘어갈 수 있도록 처리해주었다.
+ */
 public class Solved_2105_디저트_카페 {
     static int[] dx = new int[] {1, 1, -1, -1};
     static int[] dy = new int[] {1, -1, -1, 1};
@@ -11,7 +20,7 @@ public class Solved_2105_디저트_카페 {
     static int[][] arr;
     static HashSet<Integer> candidates = new HashSet<>();
 
-    static void track(int x, int y) {
+    static void track(int x, int y) { // 마름모 방향으로 일일히 다니면서 트래킹하는 함수
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
 
