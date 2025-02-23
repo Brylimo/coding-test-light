d7f30f39eafda6b12035d1dd78647db6cc756646
brylimo <icj0103@gmail.com>
2025-02-23 20:47:23 +0900

    P1038_감소하는_수

---
+++ src/Baekjoon/P1038_감소하는_수.java
@@ -0,0 +1,58 @@
+package Baekjoon;
+
+import java.sql.Array;
+import java.util.*;
+import java.io.*;
+
+public class P1038_감소하는_수 {
+    static int n, ans;
+    static int[] arr = new int[10];
+    static ArrayList<Integer> candidates = new ArrayList<>();
+    static ArrayList<String> list = new ArrayList<>();
+
+    static void calculate(int idx, String s) {
+        if (candidates.size() == 7) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < candidates.size(); i++) {
+                sb.append(candidates.get(i));
+            }
+
+            if (sb.toString() != "1000000") {
+                return;
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            if (s.length() == 0 || s.charAt(s.length() - 1) - '0' > i) {
+                list.add(new String(s + i));
+                calculate(idx + 1, new String(s + i));
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
+        }
+
+        calculate(0, "");
+
+        ArrayList<Long> alist = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            alist.add(Long.parseLong(list.get(i)));
+        }
+        Collections.sort(alist);
+
+        if (alist.size() <= n) {
+            System.out.println(-1);
+        } else {
+            System.out.println(alist.get(n));
+        }
+    }
+}
---
+++ src/Baekjoon/P3040_백설_공주와_일곱_난쟁이.java
@@ -0,0 +1,6 @@
+package Baekjoon;
+import java.util.*;
+import java.io.*;
+
+public class P3040_백설_공주와_일곱_난쟁이 {
+}
